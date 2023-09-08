2f98bb9e9772f76566ab378e1dbe691e1fb5261b
Daniel Tehrani <contact@dantehrani.com>
2023-09-08 12:57:04 +0900

    fix: fix optimized scalar mul

--- src/ecc/mul.rs
+++ src/ecc/mul.rs
@@ -19,30 +19,27 @@ pub fn ec_mul<F: FieldGC>(
 
     let minus_t_y = -t.y;
 
-    for i in (0..255).rev() {
-        let k_i = k_bits[i + 1];
-        let px = t.x;
-        let py = cs.if_then(k_i, t.y).else_then(minus_t_y);
-
-        let p = AffinePoint::new(px, py);
-
-        if i < 4 {
-            acc = ec_add_complete(ec_add_complete(p, acc, cs), acc, cs);
+    for i in 0..256 {
+        let py = if i == 0 {
+            minus_t_y
         } else {
-            acc = ec_add_incomplete(ec_add_incomplete(p, acc), acc);
-        }
+            let k_i = k_bits[256 - i];
+            cs.if_then(k_i, t.y).else_then(minus_t_y)
+        };
+
+        let p = AffinePoint::new(t.x, py);
+
+        acc = ec_add_complete(ec_add_complete(p, acc, cs), acc, cs);
     }
 
     let zero = cs.alloc_const(F::ZERO);
 
-    let final_lhs_x = cs.if_then(k_bits[0], t.x).else_then(zero);
-    let final_lhs_y = cs.if_then(k_bits[0], minus_t_y).else_then(zero);
+    let final_lhs_x = cs.if_then(k_bits[0], zero).else_then(t.x);
+    let final_lhs_y = cs.if_then(k_bits[0], zero).else_then(minus_t_y);
 
-    acc.x.print_val();
     let final_lhs = AffinePoint::new(final_lhs_x, final_lhs_y);
 
     acc = ec_add_complete(acc, final_lhs, cs);
-    acc.x.print_val();
 
     acc
 }
@@ -60,6 +57,41 @@ mod tests {
 
     use super::*;
 
+    fn double_and_add(t: Secp256k1Affine, s: Fr) -> Secp256k1Affine {
+        let mut acc = (t + t).into_affine();
+
+        // (q - 2^256) % q;
+        let t_q = Fr::from_str(
+            "115792089237316195423570985008687907852405143892509244725752742275123193348738",
+        )
+        .unwrap();
+
+        let k = s + t_q;
+
+        let k_bits = k.into_bigint().to_bits_le();
+
+        for i in 0..256 {
+            let p = if i == 0 {
+                -t
+            } else {
+                let k_i = k_bits[256 - i];
+                if k_i == true {
+                    t
+                } else {
+                    -t
+                }
+            };
+
+            acc = ((acc + p).into_affine() + acc).into_affine();
+        }
+
+        if k_bits[0] == false {
+            acc = (acc - t).into_affine();
+        }
+
+        acc
+    }
+
     #[test]
     pub fn test_mul() {
         let synthesizer = |cs: &mut ConstraintSystem<Fp>| {
@@ -71,7 +103,6 @@ mod tests {
             let p = AffinePoint::<Fp>::new(p_x, p_y);
 
             let out = ec_mul(p, &k_bits, cs);
-            out.x.print_val();
 
             cs.expose_public(out.x);
             cs.expose_public(out.y);
@@ -89,12 +120,12 @@ mod tests {
         let k = s + t_q;
         let k_bits = k
             .into_bigint()
-            .to_bits_be()
+            .to_bits_le()
             .iter()
             .map(|b| Fp::from(*b))
             .collect::<Vec<Fp>>();
 
-        let out = (p * s).into_affine();
+        let out = double_and_add(p, s);
 
         let pub_input = vec![out.x, out.y];
         let mut priv_input = vec![p.x, p.y];
@@ -102,6 +133,8 @@ mod tests {
 
         let mut cs = ConstraintSystem::new();
         let witness = cs.gen_witness(synthesizer, &pub_input, &priv_input);
-        assert!(cs.is_sat(&witness, &pub_input, synthesizer));
+
+        cs.set_constraints(&synthesizer);
+        assert!(cs.is_sat(&witness, &pub_input));
     }
 }
