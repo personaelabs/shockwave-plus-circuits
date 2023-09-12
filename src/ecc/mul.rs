d9dbe61947bd1300cec76c3cef12833b615f5218
Daniel Tehrani <contact@dantehrani.com>
2023-09-12 12:59:38 +0900

    feat: just do a naive double-and-add to make things simpler

--- src/ecc/mul.rs
+++ src/ecc/mul.rs
@@ -1,47 +1,31 @@
-use super::{
-    add::{ec_add_complete, ec_add_incomplete},
-    double::ec_double,
-    AffinePoint,
-};
+use crate::ec_double;
+
+use super::{add::ec_add_complete, AffinePoint};
 use frontend::FieldGC;
 use frontend::{ConstraintSystem, Wire};
 
-//
-// Variable-base scalar multiplication addition for secp256k1.
-// We follow the specification from the halo2 book;
-// https://zcash.github.io/halo2/design/gadgets/ecc/var-base-scalar-mul.html
+// Naive double-and-add algorithm
 pub fn ec_mul<F: FieldGC>(
-    t: AffinePoint<F>,
-    k_bits: &[Wire<F>],
+    p: AffinePoint<F>,
+    s_bits: &[Wire<F>],
     cs: &mut ConstraintSystem<F>,
 ) -> AffinePoint<F> {
-    let mut acc = ec_double(t, cs);
+    let infinity = AffinePoint::new(cs.alloc_const(F::ZERO), cs.alloc_const(F::ZERO));
+    let mut result = infinity;
+    let mut current = p;
 
-    let minus_t_y = -t.y;
+    for s_i in s_bits {
+        let t_x = *s_i * current.x;
+        let t_y = *s_i * current.y;
+        let t = AffinePoint::new(t_x, t_y);
 
-    for i in 0..256 {
-        let py = if i == 0 {
-            minus_t_y
-        } else {
-            let k_i = k_bits[256 - i];
-            cs.if_then(k_i, t.y).else_then(minus_t_y)
-        };
-
-        let p = AffinePoint::new(t.x, py);
-
-        acc = ec_add_complete(ec_add_complete(p, acc, cs), acc, cs);
+        result = ec_add_complete(t, result, cs);
+        current = ec_double(current, cs);
     }
 
-    let zero = cs.alloc_const(F::ZERO);
+    result.x.println();
 
-    let final_lhs_x = cs.if_then(k_bits[0], zero).else_then(t.x);
-    let final_lhs_y = cs.if_then(k_bits[0], zero).else_then(minus_t_y);
-
-    let final_lhs = AffinePoint::new(final_lhs_x, final_lhs_y);
-
-    acc = ec_add_complete(acc, final_lhs, cs);
-
-    acc
+    result
 }
 
 #[cfg(test)]
@@ -49,7 +33,6 @@ mod tests {
     use frontend::ark_ff::BigInteger;
     use frontend::ark_ff::PrimeField;
     use frontend::ark_secp256k1::Fr;
-    use std::str::FromStr;
 
     use ark_ec::{AffineRepr, CurveGroup};
     use frontend::ark_secp256k1::Affine as Secp256k1Affine;
@@ -57,52 +40,17 @@ mod tests {
 
     use super::*;
 
-    fn double_and_add(t: Secp256k1Affine, s: Fr) -> Secp256k1Affine {
-        let mut acc = (t + t).into_affine();
-
-        // (q - 2^256) % q;
-        let t_q = Fr::from_str(
-            "115792089237316195423570985008687907852405143892509244725752742275123193348738",
-        )
-        .unwrap();
-
-        let k = s + t_q;
-
-        let k_bits = k.into_bigint().to_bits_le();
-
-        for i in 0..256 {
-            let p = if i == 0 {
-                -t
-            } else {
-                let k_i = k_bits[256 - i];
-                if k_i == true {
-                    t
-                } else {
-                    -t
-                }
-            };
-
-            acc = ((acc + p).into_affine() + acc).into_affine();
-        }
-
-        if k_bits[0] == false {
-            acc = (acc - t).into_affine();
-        }
-
-        acc
-    }
-
     #[test]
     pub fn test_mul() {
         let synthesizer = |cs: &mut ConstraintSystem<Fp>| {
             let p_x = cs.alloc_priv_input();
             let p_y = cs.alloc_priv_input();
 
-            let k_bits = cs.alloc_priv_inputs(256);
+            let s_bits = cs.alloc_priv_inputs(256);
 
             let p = AffinePoint::<Fp>::new(p_x, p_y);
 
-            let out = ec_mul(p, &k_bits, cs);
+            let out = ec_mul(p, &s_bits, cs);
 
             cs.expose_public(out.x);
             cs.expose_public(out.y);
@@ -111,30 +59,27 @@ mod tests {
         let p = Secp256k1Affine::generator();
         let s = Fr::from(3u32);
 
-        // (q - 2^256) % q;
-        let t_q = Fr::from_str(
-            "115792089237316195423570985008687907852405143892509244725752742275123193348738",
-        )
-        .unwrap();
-
-        let k = s + t_q;
-        let k_bits = k
+        let s_bits = s
             .into_bigint()
             .to_bits_le()
             .iter()
             .map(|b| Fp::from(*b))
             .collect::<Vec<Fp>>();
 
-        let out = double_and_add(p, s);
+        let out = (p * s).into_affine();
 
         let pub_input = vec![out.x, out.y];
         let mut priv_input = vec![p.x, p.y];
-        priv_input.extend_from_slice(&k_bits);
+        priv_input.extend_from_slice(&s_bits);
 
         let mut cs = ConstraintSystem::new();
         let witness = cs.gen_witness(synthesizer, &pub_input, &priv_input);
 
         cs.set_constraints(&synthesizer);
+
+        println!("Num constraints: {}", cs.num_constraints.unwrap());
+        println!("Num vars: {}", cs.num_vars());
+
         assert!(cs.is_sat(&witness, &pub_input));
     }
 }
